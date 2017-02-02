19aa6ab979402f2b45060e46f7a99717795c53db
Guillaume <thouveng@gmail.com>
2017-02-02 13:05:23 +0100

    Add somme_des_nombres

--- bases.ml
+++ bases.ml
@@ -11,3 +11,12 @@ let preque_racine_generique f epsilon x =
     presque_zero epsilon (f x);;
 
 let presque_racine = preque_racine_generique f;;
+
+(*
+ * Return the sum between n and m by adding n, n+1, ... m
+ *)
+let rec somme_des_nombres =
+    fun n ->
+        fun m ->
+            if n > m then 0
+            else     n + (somme_des_nombres (n+1) m);;
