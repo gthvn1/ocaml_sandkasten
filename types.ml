efbf9182ce93939105d3dc3be023bf60d5226bea
Guillaume <thouveng@gmail.com>
2017-02-03 19:17:44 +0100

    Update type.ml with new exercices

--- types.ml
+++ types.ml
@@ -8,17 +8,17 @@
 (* Here is an example of sum type *)
 type couleur_de_carte = Trefle | Carreau | Coeur | Pique ;;
 
+type couleur_ou_erreur = Couleur of couleur_de_carte | Erreur ;;
+
 (* So we have a new sum type that is the union of singleton (the constructors)
  * and we can create a color like this *)
-let une_couleur = Trefle;; (* Trefle is a constructor of type couleur_de_carte *)
+let une_couleur:couleur_de_carte = Trefle;; (* Trefle is a constructor of type couleur_de_carte *)
 
 let couleur_to_string c = match c with
     | Trefle  -> "trefle"
     | Carreau -> "carreau"
     | Coeur   -> "coeur"
     | Pique   -> "pique";;
-
-couleur_to_string une_couleur;;
 
 (* We can have a more complex union combined with product type (see the last one) *)
 type carte_de_tarot =
@@ -29,6 +29,25 @@ type carte_de_tarot =
     | Cavalier of couleur_de_carte
     | Valet of couleur_de_carte
     | Nombre of int * couleur_de_carte;;
+
+let get_couleur carte = match carte with
+    | Excuse | Atout _ -> Erreur
+    | Roi c | Dame c | Cavalier c | Valet c -> Couleur c
+    | Nombre (_,c) -> Couleur c;;
+
+(* If we don't force the type to couleur_de_carte the following issue
+ * can occurs:
+ *     # est_couleur "toto" (Roi Trefle)
+ *     # bool = true
+ * It is not what we want.
+ *)
+let est_couleur (couleur:couleur_de_carte) carte = match carte with
+    | Roi couleur         -> true
+    | Dame couleur        -> true
+    | Cavalier couleur    -> true
+    | Valet couleur       -> true
+    | Nombre (_, couleur) -> true
+    | _                   -> false;;
 
 let le_petit = Atout 1;;
 
@@ -42,3 +61,5 @@ let string_of_carte_de_tarot carte = match carte with
     | Nombre (i, c) -> (string_of_int i) ^ " de " ^ (couleur_to_string c);;
 
 string_of_carte_de_tarot le_petit;;
+string_of_carte_de_tarot (Atout 5);;
+string_of_carte_de_tarot (Roi Trefle);;
