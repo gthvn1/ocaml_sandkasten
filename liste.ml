019d72df09f8c6fa6d6c81b78fb16a499a03f893
Guillaume <thouveng@gmail.com>
2017-02-06 22:53:16 +0100

    Add operations on a type called liste

--- liste.ml
+++ liste.ml
@@ -2,6 +2,31 @@ type 'a liste =
     | ListeVide
     | ListeNonVide of 'a * 'a liste;;
 
-let cons tete queue = ListeNonVide (tete, queue);;
+(* Add an element at the head *)
+let cons (tete:'a) (queue:'a liste) = ListeNonVide (tete, queue);;
 
+(* l3 = (1, 2, 3) : l3 is a list of int *)
+let l3 = cons 1 (cons 2 (cons 3 ListeVide));;
 
+(* hello = ('h', 'e', 'l', 'l', 'o') *)
+let hello = cons 'h' (cons 'e' (cons 'l' (cons 'l' (cons 'o' ListeVide))));;
+
+let est_vide = function
+    | ListeVide -> true
+    | _        -> false ;;
+
+let rec longueur = function
+    | ListeVide -> 0
+    | ListeNonVide (_, tail) -> 1 + longueur tail ;;
+
+let operation_sur_liste_droite f init =
+    let rec aux = function
+        | ListeVide                 -> init
+        | ListeNonVide (head, tail) -> (f head (aux tail))
+    in aux;;
+
+let operation_sur_liste_gauche f init =
+    let rec aux acc = function
+        | ListeVide                 -> acc
+        | ListeNonVide (head, tail) -> aux (f head acc) tail
+    in aux init;;
