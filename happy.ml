f9eade3470f60bd130e3078a297944ef274a75d8
gthvn0 <gthvn0@gmail.com>
2019-12-16 19:20:57 +0100

    Display the sum_double_digits of 32

--- happy.ml
+++ happy.ml
@@ -1,4 +1,9 @@
 let rec sum_double_digits x = 
-        if x < 10 then x * x
-        else let d = x mod 10 in
-                (d * d) + sum_double_digits (x / 10);;
+  if x < 10 then x * x
+  else let d = x mod 10 in
+    (d * d) + sum_double_digits (x / 10);;
+
+let () =
+  print_string "sum_double_digits 32 = ";
+  print_int (sum_double_digits 32);
+  print_newline()
