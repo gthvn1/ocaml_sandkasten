45299ac99af027f3772dbd0acceac771edb749b2
gthvn0 <gthvn0@gmail.com>
2019-12-24 17:09:10 +0100

    Complete day 2 by finding the noun and verb

--- advance_of_code/aoc.ml
+++ advance_of_code/aoc.ml
@@ -27,7 +27,7 @@ let prog2 = [| 1; 3; 0; 3; 99 |];;
 let prog3 = [| 1; 0; 0; 3; 1; 1; 2; 3; 99 |];;
 let prog4 = [| 1;1;1;4;99;5;6;0;99 |];;
 let prog5 =
-  [| 1;12;2;3; 1;1;2;3; 1;3;4;3; 1;5;0;3; 2;1;10;19; 2;9;19;23;
+  [| 1;0;0;3; 1;1;2;3; 1;3;4;3; 1;5;0;3; 2;1;10;19; 2;9;19;23;
      2;23;10;27; 1;6;27;31; 1;31;6;35; 2;35;10;39; 1;39;5;43;
      2;6;43;47; 2;47;10;51; 1;51;6;55; 1;55;6;59; 1;9;59;63;
      1;63;9;67; 1;67;6;71; 2;71;13;75; 1;75;5;79; 1;79;9;83;
@@ -57,21 +57,39 @@ let next_insn pos = pos + 4
  * We don't want to modify the code during the execution. So make a copy and do the
  * execution on the copy
  *)
-let execute code =
+let execute code noun verb =
   let insns = Array.copy code in
-  let rec run_insns pos =
-    let (op, a, b, c) = get_insn insns pos in
+  let rec run_insns ip =
+    let (op, a, b, c) = get_insn insns ip in
     match op with
     | Halt -> insns
-    | Add -> ( Array.set insns c (insns.(a) + insns.(b)); run_insns (next_insn pos) )
-    | Sub -> ( Array.set insns c (insns.(a) * insns.(b)); run_insns (next_insn pos) )
+    | Add -> ( Array.set insns c (insns.(a) + insns.(b)); run_insns (next_insn ip) )
+    | Sub -> ( Array.set insns c (insns.(a) * insns.(b)); run_insns (next_insn ip) )
   in
+  Array.set insns 1 noun;
+  Array.set insns 2 verb;
+  (* run the program with ip (instruction pointer) set to 0 *)
   run_insns 0
 
+exception Exit
+
 let () =
-  let executed_code = execute prog5 in
+  let executed_code = execute prog5 12 2 in
   print_string "Day1 > ";
   print_endline (totalFuel "input_day1.data");
   print_string "Day2 > ";
   print_int executed_code.(0);
-  print_newline ()
+  print_newline ();
+  print_string "Day3 > ";
+  try
+    for i = 0 to 99 do
+      for j = 0 to 99 do
+        let e = execute prog5 i j in
+        if e.(0) = 19690720 then begin
+          print_string "noun = "; print_int i;
+          print_string " verb = "; print_int j;
+          raise Exit
+        end
+      done
+    done
+  with Exit -> print_newline();
