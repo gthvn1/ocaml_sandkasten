2e75b2af0ac066eef5022c21adb0a0d929dc8990
gthvn0 <gthvn0@gmail.com>
2020-01-08 16:35:54 +0100

    run ocp-indent

--- happy/happy.ml
+++ happy/happy.ml
@@ -19,21 +19,21 @@ let rec digits_from_string = function
           (digits_from_string (String.sub s 1 ((String.length s) - 1)));;
 
 let sum_of_squares s =
-  List.fold_left (fun x y -> x + y*y) 0 (digits_from_string s);;
+  List.fold_left (fun x y -> x + y * y) 0 (digits_from_string s);;
 
 let rec list_contains l a = match l with
-    | [] -> false
-    | x::xs -> (if a = x then true
-                else list_contains xs a);;
+  | [] -> false
+  | x::xs -> (if a = x then true
+              else list_contains xs a);;
 
 let string_is_happy s =
-    let rec aux l s = match sum_of_squares s with
-        | 1 -> true
-        | y -> (if list_contains l y then false
-                else (aux (y::l) (string_of_int y))) in
-    aux [] s;;
+  let rec aux l s = match sum_of_squares s with
+    | 1 -> true
+    | y -> (if list_contains l y then false
+            else (aux (y::l) (string_of_int y))) in
+  aux [] s;;
 
 let print_is_happy s =
-    print_string s;
-    if string_is_happy s then print_endline " :)"
-    else print_endline " :(";;
+  print_string s;
+  if string_is_happy s then print_endline " :)"
+  else print_endline " :(";;
