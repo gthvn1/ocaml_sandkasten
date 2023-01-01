24db10e22d6f06ef490df54d8c01a48b11e984f5
Guillaume <gthvn1@gmail.com>
2023-01-01 16:31:46 +0100

    codingame: start merlins magic square

---
+++ codingame/merlins-magic-square/main.ml
@@ -0,0 +1,16 @@
+(* Auto-generated code below aims at helping you parse *)
+(* the standard input according to the problem statement. *)
+
+let row1 = input_line stdin in
+let row2 = input_line stdin in
+let row3 = input_line stdin in
+let allbuttonspressed = input_line stdin in
+
+(* Write an answer using print_endline *)
+(* To debug: prerr_endline "Debug message"; *)
+
+prerr_endline "row1"; prerr_endline row1;
+prerr_endline "row2"; prerr_endline row2;
+prerr_endline "row3"; prerr_endline row3;
+prerr_endline "button pressed"; prerr_endline allbuttonspressed;
+print_endline "answer (a single digit)";
