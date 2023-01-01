3381cffbea09ea5549305876fe31c3d2b9144955
Guillaume <gthvn1@gmail.com>
2023-01-01 21:10:21 +0100

    codingame: use list of button merlin
    Use a list of button and add the transformation that depends on the key
    pressed.

--- codingame/merlins-magic-square/main.ml
+++ codingame/merlins-magic-square/main.ml
@@ -1,48 +1,76 @@
-type merlin = {
-        row1: int list;
-        row2: int list;
-        row3: int list;
-}
+(* merlin has nine buttons:
+        1 2 3
+        4 5 6
+        7 8 9
+   A button is '*' of '~' that means it is lit or not
+ *)
+type button = Lit | Unlit
+type merlin = button list
 
-let int_of_lit = function
-          "~" -> 0
-        | "*" -> 1
+let lit_of_char = function
+          '~' -> Unlit
+        | '*' -> Lit
         | _ -> failwith "lit is ~ or *, nothing else"
 
-let lit_of_int = function
-          0 -> "~"
-        | _ -> "*"
+let char_of_lit = function
+          Unlit -> "~"
+        | Lit -> "*"
 
-(* transform "* ~ *" -> [1; 0; 1] *)
-let row_of_string s =
-        let slist = String.split_on_char ' ' s in
-        List.map int_of_lit slist
+(* Convert "* * *" into [Lit; Lit; Lit] *)
+let button_of_string s =
+        s
+        |> String.to_seq
+        |> List.of_seq
+        |> List.filter (fun c -> c != ' ')
+        |> List.map lit_of_char
 
-(* transform [1; 0; 1] -> "* ~ *" *)
-let string_of_row r =
-        String.concat " " (List.map lit_of_int r)
+let transform_of_button = function
+          1 -> [true ; true ; false; true ; true ; false; false; false; false]
+        | 2 -> [true ; true ; true ; false; false; false; false; false; false]
+        | 3 -> [false; true ; true ; false; true ; true ; false; false; false]
+        | 4 -> [true ; false; false; true ; false; false; true ; false; false]
+        | 5 -> [false; true ; false; true ; false; true ; false; true ; false]
+        | 6 -> [false; false; true ; false; false; true ; false; false; true ]
+        | 7 -> [false; false; false; true ; true ; false; true ; true ; false]
+        | 8 -> [false; false; false; false; false; false; true ; true ; true ]
+        | 9 -> [false; false; false; false; true ; true ; false; true ; true ]
+        | _ -> failwith "there is only 9 buttons"
+
+let transform_merlin b m =
+        let tr (x, y) = match y with
+        | Lit -> if x then Unlit else Lit
+        | Unlit -> if x then Lit else Unlit
+        in
+        List.combine (transform_of_button b) m
+        |> List.map tr
+
+(* you init merlin row by row
+ * row1 = "* * *"
+ * row2 = "~ ~ ~"
+ * row3 = "* ~ *"
+ * Res => merlin [Lit; Lit; Lit; Unlit; Unlit; Unlit; Lit; Unlit; Lit]
+ *)
+let init_merlin row1 row2 row3 =
+        button_of_string (String.concat " " [row1; row2; row3])
 
 let print_merlin m =
-        print_endline (string_of_row m.row1);
-        print_endline (string_of_row m.row2);
-        print_endline (string_of_row m.row3)
+        let c = List.map char_of_lit m in
+        Printf.printf "%s %s %s\n" (List.nth c 0) (List.nth c 1) (List.nth c 2);
+        Printf.printf "%s %s %s\n" (List.nth c 3) (List.nth c 4) (List.nth c 5);
+        Printf.printf "%s %s %s\n" (List.nth c 6) (List.nth c 7) (List.nth c 8)
 
 (* Auto-generated code below aims at helping you parse *)
 (* the standard input according to the problem statement. *)
-
 let () =
-        let input1 = input_line stdin in
-        let input2 = input_line stdin in
-        let input3 = input_line stdin in
+        let row1 = input_line stdin in
+        let row2 = input_line stdin in
+        let row3 = input_line stdin in
         let allbuttonspressed = input_line stdin in
-        let m = {
-                row1 = row_of_string input1;
-                row2 = row_of_string input2;
-                row3 = row_of_string input3;
-        } in
+        let m = init_merlin row1 row2 row3 in
         (* Write an answer using print_endline *)
         (* To debug: prerr_endline "Debug message"; *)
 
+        print_endline "----------------------";
         print_merlin m;
-        prerr_endline "button pressed"; prerr_endline allbuttonspressed;
-        print_endline "answer (a single digit)";
+
+        print_endline "answer (a single digit)"
