f49b2f14b75cdb81865fa356e642478c86da639d
Guillaume <gthvn1@gmail.com>
2023-06-22 18:28:10 +0200

    update c_bindings

--- c_bindings/hello.c
+++ c_bindings/hello.c
@@ -3,10 +3,19 @@
 #include <caml/mlvalues.h>
 #include <caml/memory.h>
 
+CAMLprim value caml_add(value v1, value v2)
+{
+    CAMLparam2(v1, v2);
+
+    int sum = Int_val(v1) + Int_val(v2);
+
+    CAMLreturn(Val_int(sum));
+}
+
 CAMLprim void caml_hello(value name)
 {
     CAMLparam1(name);
-   
+
     printf("Hello %s\n", String_val(name));
 
     CAMLreturn0;
--- c_bindings/hello.ml
+++ c_bindings/hello.ml
@@ -1,3 +1,6 @@
 external hello : string -> unit = "caml_hello"
+external myadd: int -> int -> int = "caml_add"
 
-let () = hello "toto"
+let () =
+    let res: int = myadd 40 2 in
+    hello @@ string_of_int res
