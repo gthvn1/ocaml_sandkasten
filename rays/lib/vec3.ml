5abbefd568b116847e27661f7471407ce9dd5286
Guillaume <guillaume.thouvenin@vates.tech>
2023-09-18 17:10:24 +0200

    rays: add some operations for vec3

--- rays/lib/vec3.ml
+++ rays/lib/vec3.ml
@@ -2,16 +2,23 @@ type t = float * float * float
 
 (* accessors *)
 let x (v : t) =
-  let x1, _, _ = v in
-  x1
+  let x, _, _ = v in
+  x
 
 let y (v : t) =
-  let _, y1, _ = v in
-  y1
+  let _, y, _ = v in
+  y
 
 let z (v : t) =
-  let _, _, z1 = v in
-  z1
+  let _, _, z = v in
+  z
+
+(* Operations *)
+let length_squared (v : t) : float =
+  let x, y, z = v in
+  (x *. x) +. (y *. y) +. (z *. z)
+
+let length v = length_squared v |> sqrt
 
 let ( +. ) (v1 : t) (v2 : t) : t =
   let x1, y1, z1 = v1 in
