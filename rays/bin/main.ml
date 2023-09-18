91f13591779165c1f1553860d63c614eb9737ce1
Guillaume <gthvn1@gmail.com>
2023-09-18 12:05:06 +0200

    rays: dump ppm into a /tmp/rays.ppm

--- rays/bin/main.ml
+++ rays/bin/main.ml
@@ -1,7 +1,9 @@
 open Rayslib
 
+let file = "/tmp/rays.ppm"
+
 let () =
-  (* Let's create a Green image  *)
+  let oc = open_out file in
   let image = Image.create ~width:256 ~height:256 in
   Image.transform image;
-  print_string (Image.string_of_image image)
+  Printf.fprintf oc "%s" (Image.string_of_image image)
