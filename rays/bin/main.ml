53805b88cca2e30c3d9dae5bbc8a341c40953a43
Guillaume <guillaume.thouvenin@vates.tech>
2023-09-19 15:33:16 +0200

    rays: allow to pass a filename where to dump the image

--- rays/bin/main.ml
+++ rays/bin/main.ml
@@ -1,12 +1,14 @@
 open Format
 open Rayslib
 
-let file = "/tmp/rays.ppm"
+let default_fname = "/tmp/rays.ppm"
 
 let () =
-  let oc = open_out file in
+  let fname = if (Array.length Sys.argv) < 2 then default_fname else Sys.argv.(1) in
+  let oc = open_out fname in
   let fmt = formatter_of_out_channel oc in
   let image = Ray.raytrace () in
   fprintf fmt "%s" (Image.string_of_ppm image);
+  printf "Image dumped into %s\n" fname;
   pp_print_flush fmt ();
   close_out oc
