25ab7f2a19722accd2d2355035dd4d53a6e33cc7
Guillaume <guillaume.thouvenin@vates.tech>
2023-09-20 13:33:14 +0200

    rays: use ref for default filename in main
    Not sure if it is pertinant but it allows to use ref :)

--- rays/bin/main.ml
+++ rays/bin/main.ml
@@ -1,14 +1,14 @@
 open Format
 open Rayslib
 
-let default_fname = "/tmp/rays.ppm"
+let fname = ref "/tmp/rays.ppm"
 
 let () =
-  let fname = if (Array.length Sys.argv) < 2 then default_fname else Sys.argv.(1) in
-  let oc = open_out fname in
+  let _ = if Array.length Sys.argv >= 2 then fname := Sys.argv.(1) in
+  let oc = open_out !fname in
   let fmt = formatter_of_out_channel oc in
   let image = Ray.raytrace () in
   fprintf fmt "%s" (Image.string_of_ppm image);
-  printf "Image dumped into %s\n" fname;
+  printf "Image dumped into %s\n" !fname;
   pp_print_flush fmt ();
   close_out oc
