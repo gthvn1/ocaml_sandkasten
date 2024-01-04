270c858e86d12334f9a6d5b39ad4018a6a74dab5
Guillaume <guillaume.thouvenin@vates.tech>
2024-01-04 20:42:13 +0100

    lwt_tuto: update

--- lwt_tutorial/get_inputs.ml
+++ lwt_tutorial/get_inputs.ml
@@ -1,2 +1,11 @@
-let () = print_endline "Hello"
+let read_input () =
+  print_string "Enter a number: ";
+  read_int ()
+
+let x =
+  read_input () |> fun a ->
+  read_input () |> fun b ->
+  a + b
+
+let () = print_endline (string_of_int x)
 
