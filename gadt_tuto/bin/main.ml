470808dd4e35c88e1f0c147c0eb1a7ed3c53f032
Guillaume <guillaume.thouvenin@vates.tech>
2025-04-11 18:28:00 +0200

    [gadt_tuto] renamed rpc_toy.ml into example.ml

--- gadt_tuto/bin/main.ml
+++ gadt_tuto/bin/main.ml
@@ -1,7 +1,7 @@
-let s_of_v = Gadt_tuto.Rpc_toy.string_of_value
+let s_of_v = Gadt_tuto.Example.string_of_value
 
 let () =
-  let open Gadt_tuto.Rpc_toy.Classical_variant in
+  let open Gadt_tuto.Example.Classical_variant in
   (* we want to add two integer *)
   let v1 = i 12 in
   let v2 = i 30 in
@@ -11,11 +11,11 @@ let () =
     let b1 = b true in
     plus_ v1 b1 |> eval |> s_of_v |> Printf.printf "[classical] %s\n"
     (* It compiles but it won't run *)
-  with Gadt_tuto.Rpc_toy.Ill_typed ->
+  with Gadt_tuto.Example.Ill_typed ->
     print_endline "[classical] ill typed detected at runtime"
 
 let () =
-  let open Gadt_tuto.Rpc_toy.Phantom_variant in
+  let open Gadt_tuto.Example.Phantom_variant in
   (* uncomment the block below and build. You will see that error
      is now detected at compile time .*)
   (* let _ = plus_ (i 12) (b true) in *)
