d98f86b9d53e3b69f04e0ce3086d18365440b240
Guillaume <guillaume.thouvenin@vates.tech>
2025-09-23 21:11:41 +0200

    [ocaml exercises] flatten list

--- ocaml_exercises/lib/intermediate.ml
+++ ocaml_exercises/lib/intermediate.ml
@@ -1,3 +1,24 @@
+(* ----------- FLATTEN A LIST  --------- *)
+type 'a node = One of 'a | Many of 'a node list
+
+let flatten (lst : 'a node list) : 'a list =
+  let rec aux (acc : 'a list) (l : 'a node list) =
+    match l with
+    | [] -> acc
+    | One x :: xs -> aux (x :: acc) xs
+    | Many x :: xs -> aux (aux acc x) xs
+  in
+  List.rev (aux [] lst)
+
+let a = One "a"
+let b = One "b"
+let c = One "c"
+let d = One "d";;
+
+Many [ c; d ];;
+flatten [ One "a"; Many [ One "b"; Many [ One "c"; One "d" ]; One "e" ] ]
+(* Expected : string list = ["a"; "b"; "c"; "d"; "e"] *)
+
 (* ----------- IS PRIME --------- *)
 let is_prime (n : int) : bool =
   let rec aux = function
