a3469e462074add97a060a3b5845c200e6388fc7
Guillaume <guillaume.thouvenin@vates.tech>
2025-09-23 09:05:22 +0200

    [codekata] q1 cleanup

--- codekata/bin/main.ml
+++ codekata/bin/main.ml
@@ -1,10 +1,9 @@
-module Q1 = Codekata_lib.Question1
-
 let q1 () =
+  let open Codekata_lib.Question1 in
   print_endline "1. Odd or Even Sum" ;
   ["1 2"; "8 2"; "12 31"]
   |> List.iter (fun s ->
-         Printf.printf "  > %s : sum is %s\n" s (Q1.odd_or_even s) )
+         Printf.printf "  > %s : sum is %s\n" s (odd_or_even s) )
 
 let () =
   print_endline "Starting CodeKata..." ;
