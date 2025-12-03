3b984ce2ebb4ecd0e913be34dde09ba868f3a7d2
Guillaume <guillaume.thouvenin@vates.tech>
2025-12-03 18:04:33 +0100

    Start day03

---
+++ aoc2025/src/day03.ml
@@ -0,0 +1,54 @@
+(* Convert a string of ints into a list of int.
+   Example:
+     "1234" -> [1; 2; 3; 4]
+     *)
+let string_to_ints s : int list =
+  String.to_seq s |> List.of_seq
+  |> List.map (fun c -> int_of_char c - int_of_char '0')
+
+(* Find x and if it exists return the rest of the list
+   Example:
+     [1; 2; 3; 4; 2; 3; 4] 4 => (Some 4, [2; 3; 4])
+     [1; 2; 3; 4; 2; 3; 4] 8 => (None, [])
+  *)
+let find_x (lst : int list) (value : int) : int option * int list =
+  let rec aux = function
+    | [] ->
+        (None, [])
+    | x :: xs ->
+        if x = value then (Some x, xs) else aux xs
+  in
+  aux lst
+
+(*
+  Examples:
+    In 987654321111111, => 98
+    In 811111111111119, => 89
+    In 234234234234278, => 78
+    In 818181911112111, => 92
+*)
+let find_largest (lst : int list) : int =
+  if List.length lst < 2 then failwith "At least two items are expected" ;
+  let rec aux idx a_opt b_opt l =
+    if idx < 0 then failwith "cannot be reached be keep it to catch bug" ;
+    match find_x l idx with
+    | None, _ ->
+        aux (idx - 1) a_opt b_opt l
+    | Some v, rest -> (
+      match (a_opt, b_opt) with
+      | None, None ->
+          (* Be carfull if the highest is the last one of the list, we cannot
+           build largest but as it is the bigger number keep it as the second
+           number. *)
+          if List.length rest = 0 then aux (idx - 1) None (Some v) l
+          else aux idx (Some v) None rest
+      | None, Some b ->
+          (10 * v) + b
+      | Some a, None ->
+          (10 * a) + v
+      | _ ->
+          failwith "unreachable" )
+  in
+  aux 9 None None lst
+
+let part1 () = print_endline "TODO: day3 part1"
