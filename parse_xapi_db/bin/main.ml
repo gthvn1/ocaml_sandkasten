6696cc4401c8254c20089795b12c0e737b0ad5a3
Guillaume <guillaume.thouvenin@vates.tech>
2025-12-17 15:32:52 +0100

    Start using xmlm to parse the sample.xml

--- parse_xapi_db/bin/main.ml
+++ parse_xapi_db/bin/main.ml
@@ -1,1 +1,23 @@
-let () = print_endline "TODO: parse XAPI DB"
+(*
+ https://erratique.ch/software/xmlm/doc/Xmlm/index.html
+*)
+let parse (input : Xmlm.input) =
+  try
+    let s = Xmlm.input input in
+    match s with
+    | `Dtd _ ->
+        print_endline "found dtd"
+    | `El_start _ ->
+        print_endline "start tag"
+    | `El_end ->
+        print_endline "end tag"
+    | `Data s ->
+        print_endline s
+  with _ -> print_endline "got error"
+
+let parse_xml (filename : string) =
+  let ic = open_in filename in
+  Xmlm.make_input (`Channel ic) |> parse ;
+  close_in ic
+
+let () = parse_xml "sample.xml"
