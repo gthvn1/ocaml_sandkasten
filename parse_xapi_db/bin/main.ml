659d397319d5ff4d354e8cca493d9f8afd7980a5
Guillaume <guillaume.thouvenin@vates.tech>
2025-12-17 19:01:32 +0100

    Add to sample to better understand what we want

--- parse_xapi_db/bin/main.ml
+++ parse_xapi_db/bin/main.ml
@@ -1,23 +1,3 @@
-(*
- https://erratique.ch/software/xmlm/doc/Xmlm/index.html
-*)
-let parse (input : Xmlm.input) =
-  try
-    let s = Xmlm.input input in
-    match s with
-    | `Dtd _ ->
-        print_endline "found dtd"
-    | `El_start _ ->
-        print_endline "start tag"
-    | `El_end ->
-        print_endline "end tag"
-    | `Data s ->
-        print_endline s
-  with _ -> print_endline "got error"
-
-let parse_xml (filename : string) =
-  let ic = open_in filename in
-  Xmlm.make_input (`Channel ic) |> parse ;
-  close_in ic
-
-let () = parse_xml "sample.xml"
+let () =
+  let db = Database.of_file "sample.xml" in
+  Database.print db
