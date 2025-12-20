c29c301ef91bd7bf37b2542400d25ce957b93619
Guillaume <gthvn1@gmail.com>
2025-12-20 11:07:46 +0100

    Read rom from first parameter

--- synacor/bin/main.ml
+++ synacor/bin/main.ml
@@ -1,4 +1,10 @@
 let () =
-  (* Todo: get program from first arg *)
-  let prog = Bytes.make 10 'a' in
-  Emulator.run prog
+  let prog_filename =
+    try Sys.argv.(1)
+    with _ ->
+      prerr_endline "USAGE: synacor <rom>" ;
+      exit 1
+  in
+  let ic = open_in prog_filename in
+  let rom = really_input_string ic (in_channel_length ic) in
+  Emulator.run (Bytes.of_string rom)
