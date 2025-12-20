39b80027af9e75243ce3fb922bae0be3b1aec1c1
Guillaume <gthvn1@gmail.com>
2025-12-20 14:09:47 +0100

    Simulate the whole pipeline... next execute first instruction

--- synacor/lib/emulator.ml
+++ synacor/lib/emulator.ml
@@ -7,34 +7,40 @@ let read_byte_opt (b : bytes) (pos : int) : char option =
 
 (** [decode state] returns the four bytes (chunk) under IP that is the
     maximum size of an instruction. If only one byte remains the last
-    three can be None. If there is no byte None is returned. *)
-let fetch state : chunk option * state =
+    three can be None. If there is no byte an error is raised. *)
+let fetch state : chunk * state =
   match read_byte_opt state.prog state.ip with
   | None ->
-      (None, state)
+      failwith "Failed to fetch instruction"
   | Some i ->
-      ( Some
-          ( i
-          , read_byte_opt state.prog state.ip
-          , read_byte_opt state.prog state.ip
-          , read_byte_opt state.prog state.ip )
+      ( ( i
+        , read_byte_opt state.prog state.ip
+        , read_byte_opt state.prog state.ip
+        , read_byte_opt state.prog state.ip )
       , state )
 
-let decode (fetch_step : chunk option * state) : Insn.t option * state =
-  let chunk, state =
-    match fetch_step with
-    | None, _ ->
-        failwith "Failed to fetch instruction"
-    | Some c, s ->
-        (c, s)
-  in
-  let _ = chunk in
-  let _ = state in
-  failwith "TODO: decode insn"
+let decode (fetch_step : chunk * state) : Insn.t * state =
+  let (c1, _, _, _), state = fetch_step in
+  match Insn.of_char c1 with
+  | Noop ->
+      (Noop, state)
+  | Out ->
+      failwith "TODO: out"
+  | Halt ->
+      (Halt, state)
+  | Unknown ->
+      failwith (Printf.sprintf "TODO: unknown %c" c1)
 
-let execute (decode_step : Insn.t option * state) : state =
-  let _ = decode_step in
-  failwith "todo: execute insn"
+let execute (decode_step : Insn.t * state) : state =
+  match decode_step with
+  | Noop, _ ->
+      failwith "todo: execute NOOP"
+  | Halt, _ ->
+      failwith "todo: execute HALT"
+  | Out, _ ->
+      failwith "todo: execute OUT"
+  | Unknown, _ ->
+      failwith "ERROR: unknown instruction to execute"
 
 let run (prog : bytes) : unit =
   let init_state = {prog; ip= 0} in
