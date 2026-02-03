7cd316c55d98e181515223ea0772eb9563f98748
Guillaume <gthvn1@gmail.com>
2026-02-03 21:33:34 +0100

    Allow scaling

--- mathview/main.ml
+++ mathview/main.ml
@@ -2,16 +2,14 @@
     coordinates. In screen coordinates, the origin [(0, 0)] is at the
     bottom-left corner of the screen, whereas in mathematical coordinates the
     origin is at the center of the screen. *)
-let to_screen (x, y) : int * int =
-  let scale = 50.0 in
+let to_screen (x, y) ~scale : int * int =
   let width = float_of_int @@ Graphics.size_x () in
   let height = float_of_int @@ Graphics.size_y () in
   let px = (x *. scale) +. (width /. 2.) in
   let py = (y *. scale) +. (height /. 2.) in
   (int_of_float px, int_of_float py)
 
-let from_screen (x, y) : float * float =
-  let scale = 50.0 in
+let from_screen (x, y) ~scale : float * float =
   let x = float_of_int x and y = float_of_int y in
   let width = float_of_int @@ Graphics.size_x () in
   let height = float_of_int @@ Graphics.size_y () in
@@ -29,7 +27,7 @@ let draw_axes () =
   moveto (size_x () / 2) 0;
   lineto (size_x () / 2) (size_y ())
 
-let draw_fun f ~first ~last ~color =
+let draw_fun f ~first ~last ~color ~scale =
   let open Graphics in
   set_color color;
   let step = 0.1 in
@@ -38,19 +36,20 @@ let draw_fun f ~first ~last ~color =
     if x > last then ()
     else
       let y = f x in
-      let px, py = to_screen (x, y) in
+      let px, py = to_screen (x, y) ~scale in
       lineto px py;
       loop (x +. step)
   in
   (* we go to the first position *)
-  let px, py = to_screen (first, f first) in
+  let px, py = to_screen (first, f first) ~scale in
   moveto px py;
   loop first
 
 type state = {
-    last_pressed : char option
-  ; mouse_x : int option
-  ; mouse_y : int option
+    last_pressed : char
+  ; mouse_x : int
+  ; mouse_y : int
+  ; scale : float
 }
 
 let () =
@@ -63,30 +62,29 @@ let () =
     draw_axes ();
 
     (* draw some function *)
-    draw_fun sin ~first:(-10.0) ~last:10.0 ~color:green;
+    let first, last = from_screen (0, size_x ()) ~scale:s.scale in
+    draw_fun sin ~first ~last ~color:green ~scale:s.scale;
+
+    (*
     draw_fun cos ~first:(-10.0) ~last:10.0 ~color:blue;
     draw_fun
       (fun x -> (2. *. x *. x) -. (3. *. x) +. 1.)
       ~first:(-10.0) ~last:10.0 ~color:red;
+    *)
 
     (* print the last key pressed if any *)
-    let () =
-      match s.last_pressed with
-      | None -> ()
-      | Some c ->
-          moveto 10 10;
-          draw_char c
-    in
+    set_color black;
+    moveto 10 10;
+    draw_string (Printf.sprintf "Last key pressed %c" s.last_pressed);
 
     (* display the mouse position *)
-    let () =
-      match (s.mouse_x, s.mouse_y) with
-      | Some x, Some y ->
-          let px, py = from_screen (x, y) in
-          moveto 10 30;
-          draw_string (Printf.sprintf "(%.2f,%.2f)" px py)
-      | _ -> ()
-    in
+    let px, py = from_screen (s.mouse_x, s.mouse_y) ~scale:s.scale in
+    moveto 10 30;
+    draw_string (Printf.sprintf "Mouse position (%.2f,%.2f)" px py);
+
+    (* display the scale *)
+    moveto 10 50;
+    draw_string (Printf.sprintf "Scale %.2f" s.scale);
 
     (* synchronize and wait for next event *)
     synchronize ();
@@ -95,16 +93,18 @@ let () =
     if status.keypressed then
       match status.key with
       | 'q' -> ()
+      | '+' -> loop { s with last_pressed = '+'; scale = s.scale +. 1.0 }
+      | '-' ->
+          loop { s with last_pressed = '-'; scale = max (s.scale -. 1.0) 0. }
       | c ->
           loop
             {
-              last_pressed = Some c
-            ; mouse_x = Some status.mouse_x
-            ; mouse_y = Some status.mouse_y
+              s with
+              last_pressed = c
+            ; mouse_x = status.mouse_x
+            ; mouse_y = status.mouse_y
             }
-    else
-      loop
-        { s with mouse_x = Some status.mouse_x; mouse_y = Some status.mouse_y }
+    else loop { s with mouse_x = status.mouse_x; mouse_y = status.mouse_y }
   in
-  loop { last_pressed = None; mouse_x = None; mouse_y = None };
+  loop { last_pressed = ' '; mouse_x = 0; mouse_y = 0; scale = 50.0 };
   close_graph ()
