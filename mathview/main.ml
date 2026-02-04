902437e317f96ff19ec06447d42f7c69563298ea
Guillaume <guillaume.thouvenin@vates.tech>
2026-02-04 18:16:20 +0100

    [mathview] little cleanup

--- mathview/main.ml
+++ mathview/main.ml
@@ -1,3 +1,13 @@
+type mpoint = float * float
+(** mpoint is a mathematical point *)
+
+type spoint = int * int
+(** spoint is a point on the screen *)
+
+type view = { scale : float; width : float; height : float }
+
+(* Some helpers *)
+
 let win_width () = Graphics.size_x () - 1
 let win_height () = Graphics.size_y () - 1
 
@@ -5,19 +15,15 @@ let win_height () = Graphics.size_y () - 1
     coordinates. In screen coordinates, the origin [(0, 0)] is at the
     bottom-left corner of the screen, whereas in mathematical coordinates the
     origin is at the center of the screen. *)
-let to_screen (x, y) ~scale : int * int =
-  let width = float_of_int @@ win_width () in
-  let height = float_of_int @@ win_height () in
-  let px = (x *. scale) +. (width /. 2.) in
-  let py = (y *. scale) +. (height /. 2.) in
+let to_screen (x, y) ~view : spoint =
+  let px = (x *. view.scale) +. (view.width /. 2.) in
+  let py = (y *. view.scale) +. (view.height /. 2.) in
   (int_of_float px, int_of_float py)
 
-let from_screen (x, y) ~scale : float * float =
+let from_screen (x, y) ~view : mpoint =
   let x = float_of_int x and y = float_of_int y in
-  let width = float_of_int @@ win_width () in
-  let height = float_of_int @@ win_height () in
-  let px = (x -. (width /. 2.)) /. scale in
-  let py = (y -. (height /. 2.)) /. scale in
+  let px = (x -. (view.width /. 2.)) /. view.scale in
+  let py = (y -. (view.height /. 2.)) /. view.scale in
   (px, py)
 
 let draw_axes () =
@@ -30,21 +36,19 @@ let draw_axes () =
   moveto (win_width () / 2) 0;
   lineto (win_width () / 2) (win_height ())
 
-let draw_fun f ~first ~last ~color ~scale =
+let draw_fun f ~first ~last ~color ~view ~step =
   let open Graphics in
   set_color color;
-  let step = 0.1 in
 
   let rec loop x =
     if x > last then ()
     else
-      let y = f x in
-      let px, py = to_screen (x, y) ~scale in
+      let px, py = to_screen (x, f x) ~view in
       lineto px py;
       loop (x +. step)
   in
   (* we go to the first position *)
-  let px, py = to_screen (first, f first) ~scale in
+  let px, py = to_screen (first, f first) ~view in
   moveto px py;
   loop first
 
@@ -63,18 +67,22 @@ let () =
   let rec loop s =
     clear_graph ();
 
+    (* current view *)
+    let current_width = win_width () in
+    let current_height = win_height () in
+    let view =
+      {
+        scale = s.scale
+      ; width = current_width |> float_of_int
+      ; height = current_height |> float_of_int
+      }
+    in
+
     draw_axes ();
 
     (* draw some function *)
-    let first, last = from_screen (0, win_width ()) ~scale:s.scale in
-    draw_fun sin ~first ~last ~color:green ~scale:s.scale;
-
-    (*
-    draw_fun cos ~first:(-10.0) ~last:10.0 ~color:blue;
-    draw_fun
-      (fun x -> (2. *. x *. x) -. (3. *. x) +. 1.)
-      ~first:(-10.0) ~last:10.0 ~color:red;
-    *)
+    let first, last = from_screen (0, current_width) ~view in
+    draw_fun sin ~first ~last ~color:green ~view ~step:0.1;
 
     (* print the last key pressed if any *)
     set_color black;
@@ -82,13 +90,21 @@ let () =
     draw_string (Printf.sprintf "Last key pressed %c" s.last_pressed);
 
     (* display the mouse position *)
-    let px, py = from_screen (s.mouse_x, s.mouse_y) ~scale:s.scale in
+    let px, py = from_screen (s.mouse_x, s.mouse_y) ~view in
     moveto 10 30;
     draw_string (Printf.sprintf "Mouse position (%.2f,%.2f)" px py);
 
     (* display the scale *)
     moveto 10 50;
     draw_string (Printf.sprintf "Scale %.2f" s.scale);
+
+    (* display area info *)
+    set_color blue;
+    moveto 10 70;
+    let x_min, y_min = from_screen ~view (0, 0) in
+    let x_max, y_max = from_screen ~view (current_width, current_height) in
+    draw_string
+      (Printf.sprintf "(%.2f,%.2f) -> (%.2f, %.2f)" x_min y_min x_max y_max);
 
     (* synchronize and wait for next event *)
     synchronize ();
