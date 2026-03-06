194bc382c41c80e93e05a8948f68ea76b5c03983
Guillaume <guillaume.thouvenin@vates.tech>
2026-03-06 17:36:31 +0100

    moving test in test

--- aoc2019/bin/main_day6.ml
+++
@@ -1,6 +0,0 @@
-let () =
-  let open Day6 in
-  In_channel.open_text "inputs/day6.txt"
-  |> In_channel.input_lines |> List.map parse |> map_of_orbits
-  |> get_total_orbits |> print_int;
-  print_newline ()
--- aoc2019/test/day6.t/run.t
+++ aoc2019/test/day6.t/run.t
@@ -1,2 +1,2 @@
   $ $INSIDE_DUNE/aoc2019/test/main_day6.exe
-  42
+  COM)B B)C C)D D)E E)F B)G G)H D)I E)J J)K K)L
